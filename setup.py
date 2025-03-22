d44919c287015401217a89a1def0e7af8799090b
Henry LETELLIER <henry.letellier@epitech.eu>
2025-03-22 17:56:18 +0100

    [UPDATE] the setup.py to use the dependencies located in the requirement.txt file instead of a hardcoded list

--- setup.py
+++ setup.py
@@ -4,18 +4,48 @@ File containing the required information to successfully build a python package
 
 import setuptools
 
+
+def clean_requirements(raw_requirements: list) -> list:
+    """
+    Clean the requirements list
+    """
+    r_index = 0
+    r_length = len(raw_requirements)
+    while r_index < r_length:
+        if raw_requirements[r_index].startswith("#"):
+            print(f"comment removed: {raw_requirements[r_index]}")
+            raw_requirements.pop(r_index)
+            r_length -= 1
+            continue
+        if len(raw_requirements[r_index]) == 0:
+            print(f"empty string removed: '{raw_requirements[r_index]}'")
+            raw_requirements.pop(r_index)
+            r_length -= 1
+            continue
+        if " " in raw_requirements[r_index]:
+            print(f"space removed: {raw_requirements[r_index]}")
+            raw_requirements[
+                r_index
+            ] = raw_requirements[r_index].replace(" ", "")
+        r_index += 1
+    return raw_requirements
+
+
 with open("README.md", "r", encoding="utf-8", newline="\n") as fh:
     long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8", newline="\n") as fh:
+    lib_requirements = fh.read().split("\n")
+
+lib_requirements = clean_requirements(lib_requirements)
+
+print(f"requirements = {lib_requirements}")
 
 setuptools.setup(
     name='mdi2img',
     version='1.0.0',
     packages=setuptools.find_packages(),
-    install_requires=[
-        "pillow>=10.4.0",
-        "display-tty>=1.1.5",
-        "window-asset-tkinter>=1.0.5"
-    ],
+    install_requires=lib_requirements,
     include_package_data=True,
     package_data={
         "mdi2img": ['bin/*'],
@@ -32,7 +62,7 @@ setuptools.setup(
     url="https://github.com/Hanra-s-work/MDI2IMG",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        # "License :: OSI Approved :: MIT License",
         "Operating System :: Microsoft :: Windows",
     ],
 )
